177cddbeb1c8aa5e7c21adcc03e634a900932061
bruer <f.bruer@gmail.com>
2019-04-05 11:47:38 +0200

    some changes

--- script.js
+++ script.js
@@ -4,12 +4,27 @@ $(function(){
 
     function loadProducts(){
         $.getJSON('products.json').done(function(data){
-
+            
             products = data['products'];
             console.log(products);
-    
+
+            let content = '';
+
+            for(let i = 0; i < products.length; i++){
+                    
+                content += '<div>';
+                content += '<h2>' + products[i].title + '</h2>';
+                content += '<p>price: ' + products[i].price + '</p>';
+                content += '<p>' + products[i].description + '</p>';
+                content += '</div>'
+                content += '<button id="buy-btn">buy</button>';
+            }
+            console.log(content)
+            
+            $('#products').html(content);
+
         }).fail(function(){
-            console.log('d blev fel');
+            console.log('error');
         });
     }
 
@@ -39,14 +54,5 @@ $(function(){
         if($('#chosen-product')[0].childElementCount <= 0){
             $('#chosen-product').append($(this).prev().html());
         }
-    });
-
-    $('header a').on('click', function(e){
-
-        e.preventDefault();
-        let url = this.href;
-        
-        $('#container').remove();
-        $('#content').load(url + ' #container');
     });
 });
